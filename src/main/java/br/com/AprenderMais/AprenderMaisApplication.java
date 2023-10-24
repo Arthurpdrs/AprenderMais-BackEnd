99c6544640c036d2924dc1aac513a18c0026574e
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-10-24 19:19:39 -0300

    Subindo os Primeiros Arquivos

---
+++ src/main/java/br/com/AprenderMais/AprenderMaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.AprenderMais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprenderMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprenderMaisApplication.class, args);
+	}
+
+}
