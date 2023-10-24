8793bc6a0519cd3adb5a71eb7783c213faf1bb42
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-10-24 20:42:51 -0300

    Updade GitIgnore

--- src/main/java/br/com/AprenderMais/Controller/Contole.java
+++ src/main/java/br/com/AprenderMais/Controller/Contole.java
@@ -1,7 +1,5 @@
 package br.com.AprenderMais.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
