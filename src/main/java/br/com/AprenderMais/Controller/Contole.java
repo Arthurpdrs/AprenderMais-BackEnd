d8d18f31ac58b65ccf4407ab210352b49ecd2989
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-10-24 19:55:35 -0300

    Subindo as pastas: Controller. Models, Repository e Dto

---
+++ src/main/java/br/com/AprenderMais/Controller/Contole.java
@@ -0,0 +1,10 @@
+package br.com.AprenderMais.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Contole {
+    
+}
